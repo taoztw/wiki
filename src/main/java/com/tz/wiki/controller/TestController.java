80fc785897291e00f0f77a0c1d8c79843ce0d4e9
taoztw <472311509@qq.com>
2022-03-02 19:09:38 +0800

    增加POST接口，使用HTTP client测试接口

--- src/main/java/com/tz/wiki/controller/TestController.java
+++ src/main/java/com/tz/wiki/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 //@Controller // 返回页面
 @RestController //返回数据
 public class TestController {
@@ -12,5 +14,10 @@ public class TestController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello() {
         return "Hello World!";
+    }
+
+    @RequestMapping(value = "/hello/post",method = RequestMethod.POST)
+    public String helloPost(String name) {
+        return "Hello World!" + name;
     }
 }
