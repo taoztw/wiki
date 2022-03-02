39cf9eed039d4890884cd7a3ead4c0a32c4346e6
taoztw <472311509@qq.com>
2022-03-02 19:25:43 +0800

    使用配置项，增加默认值

--- src/main/java/com/tz/wiki/controller/TestController.java
+++ src/main/java/com/tz/wiki/controller/TestController.java
@@ -1,19 +1,22 @@
 package com.tz.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 //@Controller // 返回页面
 @RestController //返回数据
 public class TestController {
 
+    // 使用配置项 :后面是默认的配置值
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @RequestMapping(value = "/hello/post",method = RequestMethod.POST)
